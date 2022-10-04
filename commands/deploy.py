0933d75d92ce08169cbd467237994a635831427b
Chris Hemming <christopher.hemming@alumni.utoronto.ca>
2022-10-04 14:42:37 +0000

    Changes to output format.

--- commands/deploy.py
+++ commands/deploy.py
@@ -9,6 +9,7 @@ import base64
 import pprint
 import shutil
 import os 
+import json
 
 target_user = 'sid' 
 
@@ -18,7 +19,7 @@ def load_pq(path,password,url_version):
     dw.load(path,password)
     accts = dw.list_accounts()
     
-    print(accts)
+    #print(accts)
     #print(dw.get_user('admin'))
     
     assert target_user in accts
@@ -61,13 +62,13 @@ def deploy_website(pq,api_key,path,name,source_path,self_id):
     with open("temp_upload.zip",'rb') as f:
         bts = f.read()
         encoded = base64.b64encode(bts).decode("ascii")       
-    print("encoded...  ", encoded[0:20])
+    #print("encoded...  ", encoded[0:20])
     remote=True
     fil  = pq.delete_entity({'api_key':api_key, 
                             'path':path, 
                             'name':name, 
                             },remote=remote) # show_url=True to debug
-    print(fil)                            
+    #print(fil)                            
     assert fil == True or 'error' in fil
 
     
@@ -80,30 +81,45 @@ def deploy_website(pq,api_key,path,name,source_path,self_id):
         'payload':encoded}
     if self_id != None:
         fil_args['self_id'] = self_id    
-    print(fil_args)
+    #print(fil_args)
     
     fil  = pq.create_entity(fil_args,remote=remote)
     #print(fil['traceback'])
-    print("upload response...  ",fil)
+    #print("upload response...  ",fil)
     assert 'obj-' in fil
     data  = pq.download_entity({'api_key':api_key,'self_id':fil , 'attrib':True},remote=remote)
-    import pprint
-    pprint.pprint(data)
+    #import pprint
+    #pprint.pprint(data)
+    print(json.dumps(data))
     return fil
-    print("Uploaded to "+fil)
+    #print("Uploaded to "+fil)
 
 
 def explain():
     return "wallet_path url_version site_dir dec_path"
 
-def run(*args):
+def get_password():
+    for prefix in ['./','../','../../']:
+        filename = prefix+".password"
+        print(filename)
+        if os.path.exists(filename):
+            f = open(filename, 'r')
+            password = f.read()
+            f.close()
+            break
+    else:
+        password = crypto.getpass()
+    print("password="+str(password))
+    return password
+
+def run(*args):    
     dir_path = os.path.dirname(os.path.realpath(__file__))    
     os.chdir(dir_path)
     #url_version = 'test.paxfinancial.ai'
-    print(type(args))
-    print(args)
-    print(type(args[0]))
-    print(args[0])
+    #print(type(args))
+    #print(args)
+    #print(type(args[0]))
+    #print(args[0])
     wallet_path = args[0]
     url_version = args[1]    
     site_dir = args[2]    
@@ -111,29 +127,32 @@ def run(*args):
     self_id = None
     if len(args) >= 5:
         self_id = args[4]
+            
     password = crypto.getpass()
+    #password = get_password()
    
     #---- begin
     root_path='/'.join(site_dir.split("/")[:-1])
     site_name = site_dir.split("/")[-1]
     website_path = '/'.join(upload_dir.split("/")[:-1])
 
-    print(root_path)
-    print(site_name)
-    print(website_path)
+    #print(root_path)
+    #print(site_name)
+    #print(website_path)
     #return
     
     [pq,api_key,wallet] = load_pq(wallet_path,password,url_version)
     #secret_passcode = wallet.get_secret('admin', 'decelium_com_dns_code')
     website_id = deploy_website(pq,api_key,root_path,site_name,website_path,self_id)
-    print("deploy_website ..."+website_id)
-        
+    #print("deploy_website ..."+website_id)
+    #print(website_id)
+    
     #if selection == "dns":
     #    deploy_dns(pq,api_key,root_path,dns_name,website_id,secret_passcode)
     #    print("deploy_dns ...")
     
-    for item in pq.list({'api_key':api_key, 'path':root_path, },remote=True):
-        print("deployed... ", item['self_id'], ' as ', item['dir_name'])
+    #for item in pq.list({'api_key':api_key, 'path':root_path, },remote=True):
+    #    print("deployed... ", item['self_id'], ' as ', item['dir_name'])
     '''
         python3 deploy.py ../../.wallet.dec dev.paxfinancial.ai  dec_path01~
     '''
