8e55a6aece9381fc845c84024c825efe83f48d17
Chris Hemming <christopher.hemming@alumni.utoronto.ca>
2022-11-14 16:51:50 +0000

    list_account.py

--- commands/list_account.py
+++ commands/list_account.py
@@ -44,8 +44,9 @@ if __name__ == "__main__":
     password = crypto.getpass()
  
     [pq,api_key,wallet] = load_pq(wallet_path,password,url_version,target_user)
-    print({'api_key':api_key, 'path':root_directory, })
-    for item in pq.list({'api_key':api_key, 'path':root_directory, },remote=True):
+    q={'api_key':api_key, 'path':root_directory, }
+    print(q)
+    for item in pq.list(q,remote=True):
         #print(item['error'])
         print("deployed... ", item['self_id'], ' as ', item['dir_name'])
         
