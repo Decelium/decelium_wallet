13704b59192c8d87f1dbe52fabecf5d3c37775aa
Chris Hemming <christopher.hemming@alumni.utoronto.ca>
2023-02-07 22:04:20 +0000

    setup.py file created

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='decelium_wallet',
+    version='0.1.0',    
+    description='The Decelium wallet',
+    url='https://github.com/Decelium/decelium_wallet.git',
+    author='Justin Girard',
+    author_email='justin.girard@justingirard.com',
+    packages=['decelium_wallet'],
+    install_requires=[ 'ecdsa',
+                        'cryptography',
+                        'flask',
+                        'pandas',
+                        'zipfile36'
+                      ],
+)
