52974dfcfc02f281d4cfcd5ccb1a17db755c15f1
Chris Hemming <christopher.hemming@alumni.utoronto.ca>
2023-03-28 15:25:37 +0000

    test_python.py

--- __tests__/python/test_python.py
+++ __tests__/python/test_python.py
@@ -9,23 +9,48 @@ import decelium_wallet.commands.delete_user as delete_user
 import uuid
 import requests
 import sys
+import pprint
+import traceback
 
 try:
-    generate_a_wallet.run("./test_wallet.dec")
-    generate_user.run("./test_wallet.dec","test_user","confirm")
+    wallet=generate_a_wallet.run("./test_wallet.dec")
+    assert len(wallet)==0
+    
+    wallet=generate_user.run("./test_wallet.dec","test_user","confirm")
+    assert "test_user" in wallet
+    assert "description" in wallet["test_user"]
+    assert "image" in wallet["test_user"]
+    assert "secrets" in wallet["test_user"]
+    assert "title" in wallet["test_user"]
+    assert "user" in wallet["test_user"]
+    assert "api_key" in wallet["test_user"]["user"]
+    assert "private_key" in wallet["test_user"]["user"]
+    assert "version" in wallet["test_user"]["user"]
+    
     test_username="test_user"+str(uuid.uuid4())
-    create_user.run("./test_wallet.dec","test_user",test_username,"test.paxfinancial.ai","passtest")
-    fund.run("./test_wallet.dec","test_user","test.paxfinancial.ai")
-    check_balance.run("./test_wallet.dec","test_user","test.paxfinancial.ai")
+    user_id=create_user.run("./test_wallet.dec","test_user",test_username,"test.paxfinancial.ai","passtest")
+    assert "obj-" in user_id
+    
+    fund_result=fund.run("./test_wallet.dec","test_user","test.paxfinancial.ai")
+    assert fund_result
+    
+    balance=check_balance.run("./test_wallet.dec","test_user","test.paxfinancial.ai")
+    assert balance==200    
+    
     website_id = deploy.run("./test_wallet.dec","test_user","test.paxfinancial.ai","test/example_small_website.ipfs","./website/")
     url = "https://test.paxfinancial.ai/obj/"+website_id+"/"
+    assert "obj-" in website_id
     response = requests.get(url)
     assert response.status_code==200
     assert response.content==b'<!DOCTYPE html>\n<html>\n<body>\n\n<p>This text is normal.</p>\n\n<p><em>This text is emphasized.</em></p>\n\n</body>\n</html>\n'
+    
     del_result=delete_user.run("./test_wallet.dec","test_user",test_username,"test.paxfinancial.ai")
     assert del_result==True
-except:
+
+except Exception as e:
+    traceback.print_exc()
     print("Exiting with status 1")
     sys.exit(1)
+
 print("Exiting with status 0")
 sys.exit(0)
