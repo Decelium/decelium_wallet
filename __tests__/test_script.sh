84b932eb364c79278a77cb99eca2f83eb7a17e05
Chris Hemming <christopher.hemming@alumni.utoronto.ca>
2023-03-30 14:43:20 +0000

    reformatted test_script.sh so it won't crash if one test crashes

--- __tests__/test_script.sh
+++ __tests__/test_script.sh
@@ -1,5 +1,24 @@
-cd js && echo "testing js" && python3 test_js.py && cd ..
-cd nodejs && echo "testing nodejs" && node test_nodejs.js && cd ..
-cd reactjs && echo "testing reactjs" && python3 test_reactjs.py && cd ..
-cd cli && echo "testing cli" && python3 test_cli.py && cd ..
-#cd python && echo "testing python" && python3 test_python.py && cd ..
+cd js 
+echo "testing js"
+python3 test_js.py 
+cd ..
+
+cd nodejs 
+echo "testing nodejs"
+node test_nodejs.js 
+cd ..
+
+cd reactjs
+echo "testing reactjs"
+python3 test_reactjs.py
+cd ..
+
+cd cli
+echo "testing cli" 
+python3 test_cli.py
+cd ..
+
+#cd python
+#echo "testing python"
+#python3 test_python.py
+#cd ..
