88934f34bc11f243a7555768c6dbe286e90bdcd7
Pocket Jake <justingirard@justingirard.com>
2023-04-08 12:40:45 -0400

    Create py_build.js

---
+++ decelium_wallet/py_build.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const path = require('path');
+
+const pythonFiles = ['crypto.py', 'decelium.py', 'wallet.py'];
+
+pythonFiles.forEach((file) => {
+  const filePath = path.join(__dirname, file);
+  const content = fs.readFileSync(filePath, 'utf8');
+  const jsFilePath = path.join(__dirname, `${file}.js`);
+  const jsContent = `export default \`${content}\`;\n`;
+  fs.writeFileSync(jsFilePath, jsContent);
+});
