c7b1c36b41ad0b1b0ac3f6aeb3b4f5a3ecce4a0d
Pocket Jake <justingirard@justingirard.com>
2023-04-08 13:26:14 -0400

    Update crypto.py

--- decelium_wallet/crypto.py
+++ decelium_wallet/crypto.py
@@ -40,7 +40,6 @@ class crypto:
         password = None
         while password == None and len(path)<=3*4: 
             try:
-                #sys.stderr.write(os.path.abspath(path+'.password\n'))
                 with open(path+'.password','r') as f:
                     password =  f.read()
                     return password.strip()
