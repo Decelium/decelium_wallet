3ca9de8d5db5c7494b825ec65af7ae2c3fc27bb1
Devel Oper <developer@decelium.ai>
2023-11-01 00:39:14 +0000

    generalizing nodejs into node+browser

--- __tests__/js/test_project/src/main.js
+++ __tests__/js/test_project/src/main.js
@@ -1,7 +1,9 @@
 //import * as IPFS from 'ipfs-core';
-import ipfsClient from 'ipfs-http-client';
+//import ipfsClient from 'ipfs-http-client';
 //import * as IPFS from 'ipfs-core/src/index.js';
 import { helloWorld } from './hello';
 
 //import Core from "../../../../decelium_wallet/core.js";
+
+
 document.getElementById('hello-message').innerText = helloWorld();
