a7dd511107a5122529ee6920738983ead8e238ce
Some Name <developer@computer.com>
2024-06-18 15:09:26 -0400

    mas

---
+++ __init__.py
@@ -0,0 +1,16 @@
+#from .decelium_wallet.wallet import wallet 
+#from .decelium_wallet.network import network
+#from .decelium_wallet.service import service
+#from .decelium_wallet import core
+#from .decelium_wallet import crypto
+
+#import os
+#import sys
+
+# Append the directory containing the actual package to the system path
+#current_dir = os.path.dirname(os.path.abspath(__file__))
+#package_dir = os.path.join(current_dir, 'decelium_wallet')
+#sys.path.append(package_dir)
+#print("wallet appended "+package_dir)
+# Now you can import the inner package
+#from .decelium_wallet import *
